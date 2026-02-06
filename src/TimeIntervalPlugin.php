2e7d63ff49cccae4543ca72281d5ab558d53793f
nojimage <nojimage@gmail.com>
2026-02-06 23:54:35 +0900

    cake53: Refactor Plugin class to TimeIntervalPlugin and handle TypeFactory deprecation

--- src/TimeIntervalPlugin.php
+++ src/TimeIntervalPlugin.php
@@ -17,7 +17,7 @@ use Elastic\TimeInterval\Validation\TimeIntervalValidation;
 /**
  * Plugin class for CakePHP.
  */
-class Plugin extends BasePlugin
+class TimeIntervalPlugin extends BasePlugin
 {
     /**
      * @inheritDoc
@@ -46,11 +46,26 @@ class Plugin extends BasePlugin
      */
     public function bootstrap(PluginApplicationInterface $app): void
     {
-        if (!TypeFactory::getMap('time_interval')) {
+        if (!self::hasTimeIntervalTypeMap()) {
             TypeFactory::map('time_interval', TimeIntervalType::class);
             TypeFactory::map('time_interval_int', TimeIntervalAsIntType::class);
         }
 
         Validator::addDefaultProvider('timeInterval', new TimeIntervalValidation());
+    }
+
+    /**
+     * Checks whether a time interval type map has been registered in the `TypeFactory`.
+     *
+     * @return bool True if the time interval type map exists, false otherwise.
+     */
+    private static function hasTimeIntervalTypeMap(): bool
+    {
+        // since CakePHP 5.3.0, TypeFactory::getMap() is deprecated
+        if (method_exists(TypeFactory::class, 'getMapped')) {
+            return (bool)TypeFactory::getMapped('time_interval');
+        }
+
+        return (bool)TypeFactory::getMap('time_interval');
     }
 }
